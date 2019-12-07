97a96be43731eae9cd9a37de88ffbec463dece9a
Dominik Charousset <dominik@charousset.de>
2019-12-07 22:31:41 +0100

    Drop legacy code, use more C++17 if constexpr

--- libcaf_core/caf/match_case.hpp
+++ libcaf_core/caf/match_case.hpp
@@ -29,7 +29,7 @@
 #include "caf/detail/try_match.hpp"
 #include "caf/detail/type_list.hpp"
 #include "caf/detail/type_traits.hpp"
-#include "caf/match_case.hpp"
+#include "caf/message.hpp"
 #include "caf/none.hpp"
 #include "caf/optional.hpp"
 #include "caf/param.hpp"
@@ -41,10 +41,11 @@ class CAF_CORE_EXPORT match_case {
 public:
   enum result { no_match, match, skip };
 
-  match_case(uint32_t tt);
+  explicit match_case(uint32_t tt);
 
-  match_case(match_case&&) = default;
-  match_case(const match_case&) = default;
+  match_case(const match_case&) = delete;
+
+  match_case& operator=(const match_case&) = delete;
 
   virtual ~match_case();
 
@@ -53,45 +54,12 @@ public:
   invoke(detail::invoke_result_visitor& rv, type_erased_tuple& xs)
     = 0;
 
-  inline uint32_t type_token() const {
+  uint32_t type_token() const noexcept {
     return token_;
   }
 
 private:
   uint32_t token_;
-};
-
-template <bool IsVoid, class F>
-class lfinvoker {
-public:
-  lfinvoker(F& fun) : fun_(fun) {
-    // nop
-  }
-
-  template <class... Ts>
-  typename detail::get_callable_trait<F>::result_type operator()(Ts&&... xs) {
-    return fun_(std::forward<Ts>(xs)...);
-  }
-
-private:
-  F& fun_;
-};
-
-template <class F>
-class lfinvoker<true, F> {
-public:
-  lfinvoker(F& fun) : fun_(fun) {
-    // nop
-  }
-
-  template <class... Ts>
-  unit_t operator()(Ts&&... xs) {
-    fun_(std::forward<Ts>(xs)...);
-    return unit;
-  }
-
-private:
-  F& fun_;
 };
 
 template <class F>
@@ -115,37 +83,41 @@ public:
   using decayed_arg_types =
     typename detail::tl_map<arg_types, std::decay>::type;
 
-  using intermediate_pseudo_tuple =
-    typename detail::tl_apply<decayed_arg_types, detail::pseudo_tuple>::type;
+  using intermediate_pseudo_tuple
+    = detail::tl_apply_t<decayed_arg_types, detail::pseudo_tuple>;
 
-  trivial_match_case(trivial_match_case&&) = default;
-  trivial_match_case(const trivial_match_case&) = default;
-  trivial_match_case& operator=(trivial_match_case&&) = default;
-  trivial_match_case& operator=(const trivial_match_case&) = default;
+  trivial_match_case(const trivial_match_case&) = delete;
 
-  trivial_match_case(F f)
+  trivial_match_case& operator=(const trivial_match_case&) = delete;
+
+  explicit trivial_match_case(F f)
     : match_case(make_type_token_from_list<pattern>()), fun_(std::move(f)) {
     // nop
   }
 
   match_case::result
-  invoke(detail::invoke_result_visitor& f, type_erased_tuple& xs) override {
+  invoke(detail::invoke_result_visitor& f, type_erased_tuple& xs) {
     detail::meta_elements<pattern> ms;
     // check if try_match() reports success
     if (!detail::try_match(xs, ms.arr.data(), ms.arr.size()))
       return match_case::no_match;
     typename detail::il_indices<decayed_arg_types>::type indices;
-    lfinvoker<std::is_same<result_type, void>::value, F> fun{fun_};
     message tmp;
     auto needs_detaching = is_manipulator && xs.shared();
     if (needs_detaching)
       tmp = message::copy(xs);
     intermediate_pseudo_tuple tup{needs_detaching ? tmp.content() : xs};
-    auto fun_res = apply_args(fun, indices, tup);
-    return f.visit(fun_res) ? match_case::match : match_case::skip;
+    if constexpr (std::is_same<result_type, void>::value) {
+      apply_args(fun_, indices, tup);
+      auto fun_res = unit;
+      return f.visit(fun_res) ? match_case::match : match_case::skip;
+    } else {
+      auto fun_res = apply_args(fun_, indices, tup);
+      return f.visit(fun_res) ? match_case::match : match_case::skip;
+    }
   }
 
-protected:
+private:
   F fun_;
 };
 
@@ -156,36 +128,6 @@ struct match_case_info {
 
 inline bool operator<(const match_case_info& x, const match_case_info& y) {
   return x.type_token < y.type_token;
-}
-
-template <class F>
-typename std::enable_if<!std::is_base_of<match_case, F>::value,
-                        std::tuple<trivial_match_case<F>>>::type
-to_match_case_tuple(F fun) {
-  return std::make_tuple(std::move(fun));
-}
-
-template <class MatchCase>
-typename std::enable_if<std::is_base_of<match_case, MatchCase>::value,
-                        std::tuple<const MatchCase&>>::type
-to_match_case_tuple(const MatchCase& x) {
-  return std::tie(x);
-}
-
-template <class... Ts>
-const std::tuple<Ts...>& to_match_case_tuple(const std::tuple<Ts...>& x) {
-  static_assert(
-    detail::conjunction<std::is_base_of<match_case, Ts>::value...>::value,
-    "to_match_case_tuple received a tuple of non-match_case Ts");
-  return x;
-}
-
-template <class T, class U>
-typename std::enable_if<std::is_base_of<match_case, T>::value || std::is_base_of<match_case, U>::value>::type operator,(
-  T, U) {
-  static_assert(!std::is_same<T, T>::value,
-                "this syntax is not supported -> you probably did "
-                "something like 'return (...)' instead of 'return {...}'");
 }
 
 } // namespace caf
