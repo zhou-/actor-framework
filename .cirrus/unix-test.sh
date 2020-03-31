7d6535527fb7f93f69d5e99673885c7ab9ef3475
Dominik Charousset <dominik@charousset.de>
2020-03-31 17:15:37 +0200

    Use build matrices in cirrus build

--- .cirrus/unix-test.sh
+++ .cirrus/unix-test.sh
@@ -2,9 +2,9 @@
 
 # prefer cmake3 over "regular" cmake (cmake == cmake2 on RHEL)
 if command -v ctest3 >/dev/null 2>&1 ; then
-  CMakeCommand="ctest3"
+  CTestCommand="ctest3"
 else
-  CMakeCommand="ctest"
+  CTestCommand="ctest"
 fi
 
 cd build
